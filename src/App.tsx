77eb3d48e778ee83b882c392a462d24a6470e089
Tuan Khang <damtuankhangctz@gmail.com>
2023-07-11 00:03:10 +0700

    func call api logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
 
 function App() {
-  const myName = "khang dev"
+  const myName = "khang dev";
+  const logoutFunc = () => {
+    console.log("call api logout");
+  };
 
   return <div>Hello world ammend</div>;
 }
